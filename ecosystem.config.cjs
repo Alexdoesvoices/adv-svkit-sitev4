6ea3e48d13eed6d229d178f5d85c27a91a8bd11f
Alex Robinson <alexrrobinson05@gmail.com>
2026-04-06 12:46:29 -0500

    updated pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,14 +2,16 @@ module.exports = {
 	apps: [
 		{
 			name: 'adv-svkit-sitev4',
-			script: 'build/index.js',
+			script: 'bun',
 			env: {
 				NODE_ENV: 'production',
-				PORT: 3000,
-				ORIGIN: 'http://localhost',
+				PORT: 8118,
+				ORIGIN: 'http://alexdoesvoices.com',
 				ADDRESS_HEADER: 'x-forwarded-for',
 				PROTOCOL_HEADER: 'x-forwarded-proto'
-			}
-		}
+			},
+		restart_delay: 5000,
+		max_memory_restart: '500M'
+	  }
 	]
 };

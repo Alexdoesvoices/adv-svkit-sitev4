75b0c85d83da6a0b3206c25d46665bbb29a2c329
Alex Robinson <alexrrobinson05@gmail.com>
2026-04-09 12:28:23 -0500

    differnt port maybe

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
 		cwd: '/home/arobinson/dev/projects/adv-svkit-sitev4',
 		env: {
 		  NODE_ENV: 'production',
-		  PORT: 8118,
+		  PORT: 7109,
 		  HOST: '0.0.0.0', 
 		  ORIGIN: 'https://alexdoesvoices.com',
 		  ADDRESS_HEADER: 'x-forwarded-for',

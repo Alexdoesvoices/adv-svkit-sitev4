56d5d479e6784ceb474b53da96efcff293cd24de
Alex Robinson <alexrrobinson05@gmail.com>
2026-04-14 19:48:12 -0500

    ran prettier format

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,20 +1,20 @@
 module.exports = {
 	apps: [
-	  {
-		name: 'adv-svkit-sitev4',
-		script: 'bun',
-		args: 'run /home/arobinson/dev/projects/adv-svkit-sitev4/build/index.js',
-		cwd: '/home/arobinson/dev/projects/adv-svkit-sitev4',
-		env: {
-		  NODE_ENV: 'production',
-		  PORT: 7109,
-		  HOST: '0.0.0.0', 
-		  ORIGIN: 'https://alexdoesvoices.com',
-		  ADDRESS_HEADER: 'x-forwarded-for',
-		  PROTOCOL_HEADER: 'x-forwarded-proto'
-		},
-		restart_delay: 5000,
-		max_memory_restart: '500M'
-	  }
+		{
+			name: 'adv-svkit-sitev4',
+			script: 'bun',
+			args: 'run /home/arobinson/dev/projects/adv-svkit-sitev4/build/index.js',
+			cwd: '/home/arobinson/dev/projects/adv-svkit-sitev4',
+			env: {
+				NODE_ENV: 'production',
+				PORT: 7109,
+				HOST: '0.0.0.0',
+				ORIGIN: 'https://alexdoesvoices.com',
+				ADDRESS_HEADER: 'x-forwarded-for',
+				PROTOCOL_HEADER: 'x-forwarded-proto'
+			},
+			restart_delay: 5000,
+			max_memory_restart: '500M'
+		}
 	]
-  }
+};
